a12e7df51a65962b13cef7ec9ebb71d35188a04e
elEveQer <161108477+elEveQer@users.noreply.github.com>
2024-10-27 22:11:50 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,37 @@
-
 const Header = () => {
   return (
-
-    export const Header = ({handleMostrar}) => {
-      return (
     <header className="bg-gradient-to-b from-blue-900 to-black">
-      <div className="container mx-auto flex justify-between items-center p-4">
+      <div className="container mx-auto flex items-center p-4">
         <div className="flex items-center">
           <img
             src="image.png" 
-            alt="Logo"
+            alt="INTERPROCSYS Logo"
             className="h-12 w-12 mr-2"
           />
-          <h1 className="text-2xl font-bold text-white">INTERPROCSYS</h1>
+          <h1 className="text-[36px] font-poppins font-bold text-white">INTERPROCSYS</h1>
         </div>
-        
-        <nav>
-          
-          <ul className="flex space-x-4">
+        <nav className="flex-grow flex justify-center">
+          <ul className="flex space-x-6">
             <li>
-              <a href="#servicio" className="text-white">servicio</a>
+              <a href="#servicio" className="text-white">Servicio</a>
             </li>
             <li>
-              <a href="#empresa" className="text-white">empresa</a>
+              <a href="#empresa" className="text-white">Empresa</a>
             </li>
             <li>
-              <a href="#entrevista" className="text-white">entrevista</a>
+              <a href="#entrevista" className="text-white">Entrevista</a>
             </li>
           </ul>
         </nav>
       </div>
-
       <div className="flex items-center justify-center h-screen">
-        <div className="flex flex-col items-center mr-6">
-          <h1 className="text-6xl text-white text-center">
-            Expertos
-           
-            <p className="text-blue-400">en</p>
-           
-            <p className="text-blue-500">Tecnología</p>
-            
-            <p className="text-blue-300">4.0</p>
-            
-            <p className="text-cyan-400">y Blockshop</p>
-          </h1>
-          <button
-        onClick={handleMostrar}
-          className="px-4 py-2 ml-4 text-white transition duration-300 bg-blue-500 rounded hover:bg-blue-600"
-        ></button>
+  <div className="flex flex-col items-center">
+    <h2 className="text-[60px] font-poppins font-medium text-white text-center mb-2">
+      Expertos en <span className="text-blue-300">Tecnología</span>
+    </h2>
+    <p className="text-[60px] font-poppins font-medium text-blue-500 mb-4">
+      <span className="text-blue-500">4.0</span> y <span className="text-blue-500">Blockchain</span>
+    </p>
           <p className="text-lg text-white mt-4 text-center">
             Transforma tu negocio hoy con soluciones innovadoras y personalizadas
           </p>
