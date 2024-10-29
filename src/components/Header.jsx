027597bc94b221aace56a7e2c433bd39ebf00187
elEveQer <161108477+elEveQer@users.noreply.github.com>
2024-10-28 21:46:12 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
       <div className="container mx-auto flex items-center justify-between p-4">
         <div className="flex items-center">
           <img
-            src="image.png" 
+            src="ips-adobbe.webp" 
             alt="INTERPROCSYS Logo"
             className="h-12 w-12 mr-2"
           />
@@ -23,48 +23,46 @@ const Header = () => {
 
         <div className="md:hidden">
           <button onClick={toggleMenu} className="text-white focus:outline-none">
-            {isOpen ? '✖️' : 'Entrevistas'}
+            {isOpen ? '✖️' : 'Entrevistas>'}
           </button>
         </div>
 
         <nav className={`flex-grow md:flex ${isOpen ? 'block' : 'hidden'} md:block`}>
-          <ul className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-7">
-            <li>
-              <a href="#servicio" className="text-white" aria-label="Servicio">Servicio</a>
-            </li>
-            <li>
-              <a href="#empresa" className="text-white" aria-label="Empresa">Empresa</a>
-            </li>
-            <li>
-              <a href="#entrevista" className="text-white" aria-label="Entrevista">Entrevista</a>
-            </li>
-          </ul>
+          <div className="flex justify-center md:justify-end md:ml-8"> 
+            <ul className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-7">
+              <li>
+                <a href="#servicio" className="text-white" aria-label="Servicio">Servicio</a>
+              </li>
+              <li>
+                <a href="#empresa" className="text-white" aria-label="Empresa">Empresa</a>
+              </li>
+              <li>
+                <a href="#entrevista" className="text-white" aria-label="Entrevista">Entrevista</a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
 
-      {/* Sección Principal */}
       <div className="flex flex-col items-center justify-center h-screen text-center">
-        <div className="flex flex-col md:flex-row items-center justify-center">
-          <h2 className="text-[60px] font-poppins font-medium text-white mb-2">
-            Expertos en <span className="text-blue-300">Tecnología</span>
-          </h2>
-          <button 
-            onClick={handleContactClick}
-            className="bg-cyan-500 text-black py-4 px-8 text-lg rounded hover:bg-cyan-600 transition duration-300 shadow-md md:ml-4 mt-4 md:mt-0"
-            aria-label="Contáctanos"
-          >
-            Contáctanos
-          </button>
-        </div>
+        <h2 className="text-[60px] font-poppins font-medium text-white mb-2">
+          Expertos en <span className="text-blue-300">Tecnología</span>
+        </h2>
         <p className="text-[60px] font-poppins font-medium text-blue-500 mb-4">
           <span className="text-blue-500">4.0</span> y <span className="text-blue-500">Blockchain</span>
         </p>
         <p className="text-lg text-white mt-4">
           Transforma tu negocio hoy con soluciones innovadoras y personalizadas
         </p>
+        <button 
+          onClick={handleContactClick}
+          className="bg-cyan-500 text-black py-4 px-8 text-lg rounded hover:bg-cyan-600 transition duration-300 shadow-md mt-4"
+          aria-label="Contáctanos"
+        >
+          Contáctanos
+        </button>
       </div>  
 
-    
       <div className="relative">
         <svg 
           className="w-full"
@@ -85,4 +83,3 @@ const Header = () => {
 };
 
 export default Header;
-
