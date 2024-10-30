404bf95f67c7ad2e669adc89d49be2c4c86e9e58
elEveQer <161108477+elEveQer@users.noreply.github.com>
2024-10-29 20:25:23 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,62 +1,79 @@
-const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+import { Link, useNavigate } from "react-router-dom"; // Importa Link y useNavigate
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+export const Header = ({ handleMostrar }) => {
+  const [fade, setFade] = useState(false);
+  const navigate = useNavigate();
 
-  const handleContactClick = () => {
-    alert('');
+  const handleClick = (path) => {
+    setFade(true);
+    setTimeout(() => {
+      navigate(path);
+      setFade(false); 
+    }, 500); 
   };
 
   return (
     <header className="bg-gradient-to-b from-blue-900 to-black">
-      <div className="container mx-auto flex items-center justify-between p-4">
+      <div className="container flex items-center justify-between p-4 mx-auto">
         <div className="flex items-center">
           <img
             src="ips-adobbe.webp" 
             alt="INTERPROCSYS Logo"
-            className="h-12 w-12 mr-2"
+            className="w-12 h-12 mr-2"
           />
-          <h1 className="text-[36px] font-poppins font-bold text-white">INTERPROCSYS</h1>
+          <h1 className="text-[30px] font-poppins font-bold text-white">INTERPROCSYS</h1>
         </div>
 
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
-            {isOpen ? '✖️' : 'Entrevistas>'}
+          <button onClick={handleMostrar} className="text-white focus:outline-none">
+            Contacto
           </button>
         </div>
 
-        <nav className={`flex-grow md:flex ${isOpen ? 'block' : 'hidden'} md:block`}>
+        <nav className={`flex-grow md:flex`}>
           <div className="flex justify-center md:justify-end md:ml-8"> 
-            <ul className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-7">
+            <ul className="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-7">
               <li>
-                <a href="#servicio" className="text-white" aria-label="Servicio">Servicio</a>
+              <a
+                  href="#"
+                  onClick={() => handleSectionClick('servicio')}
+                  className="text-white hover:text-[#4E99A8] font-poppins"
+                  aria-label="Servicio"
+                >
+                  Servicio
+                </a>
               </li>
               <li>
-                <a href="#empresa" className="text-white" aria-label="Empresa">Empresa</a>
+              <a
+                  href="#"
+                  onClick={() => handleSectionClick('empresa')}
+                  className="text-white hover:text-[#4E99A8] font-poppins"
+                  aria-label="Empresa"
+                >
+                  Empresa
+                </a>
               </li>
               <li>
-                <a href="#entrevista" className="text-white" aria-label="Entrevista">Entrevista</a>
+                <Link to="/entrevistas" className="text-white" aria-label="Entrevista">Entrevista</Link>
               </li>
             </ul>
           </div>
         </nav>
       </div>
 
-      <div className="flex flex-col items-center justify-center h-screen text-center">
+      <div className={`flex flex-col items-center justify-center h-screen text-center transition-opacity duration-500 ${fade ? 'opacity-0' : 'opacity-100'}`}>
         <h2 className="text-[60px] font-poppins font-medium text-white mb-2">
           Expertos en <span className="text-blue-300">Tecnología</span>
         </h2>
         <p className="text-[60px] font-poppins font-medium text-blue-500 mb-4">
           <span className="text-blue-500">4.0</span> y <span className="text-blue-500">Blockchain</span>
         </p>
-        <p className="text-lg text-white mt-4">
+        <p className="mt-4 text-lg text-white">
           Transforma tu negocio hoy con soluciones innovadoras y personalizadas
         </p>
         <button 
-          onClick={handleContactClick}
-          className="bg-cyan-500 text-black py-4 px-8 text-lg rounded hover:bg-cyan-600 transition duration-300 shadow-md mt-4"
+          onClick={handleMostrar}
+          className="px-8 py-4 mt-4 text-lg text-black transition duration-300 rounded shadow-md bg-cyan-500 hover:bg-cyan-600"
           aria-label="Contáctanos"
         >
           Contáctanos
@@ -81,5 +98,3 @@ const Header = () => {
     </header>
   );
 };
-
-export default Header;
